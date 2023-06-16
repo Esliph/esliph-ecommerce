c8a4eb19ba687426855a5b87145af69066871b58
Dan Ruan <101991292+Danrley-Ruan-Saquetti@users.noreply.github.com>
2023-06-16 11:52:01 +0000

    update schema

---
+++ src/app/modules/people/people.module.ts
@@ -0,0 +1,13 @@
+import { Module } from '@nestjs/common'
+import { PeopleController } from '@modules/people/controller'
+import { PeopleUseCaseModule } from '@modules/people/use-case.module'
+import { PeopleRepositoryModule } from '@modules/people/repository.module'
+
+@Module({
+    imports: [
+        PeopleRepositoryModule,
+        PeopleUseCaseModule
+    ],
+    controllers: [PeopleController],
+})
+export class PeopleModule { }
